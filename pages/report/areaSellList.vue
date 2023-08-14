9de31f4066cc8a11536f384a9210650e9a9106b4
CHENXIAOXIN\83745 <873456296@qq.com>
2023-08-14 15:23:22 +0800

    筛选

--- pages/report/areaSellList.vue
+++ pages/report/areaSellList.vue
@@ -69,27 +69,17 @@
       </scroll-view>
     </view>
 
-    <u-datetime-picker
-                title="开始时间"
-                :show="showFrom"
-        v-model="dateCurrent"
-        mode="year-month"
-        :minDate="1646064000000"
-        @confirm="confirmFrom"
-        @cancel="cancelFrom"
-    ></u-datetime-picker>
-    <u-datetime-picker
-       title="结束时间"c
-                :show="showTo"
-        v-model="dateCurrent"
-        mode="year-month"
-        :minDate="1646064000000"
-        @confirm="confirmTo"
-        @cancel="cancelTo"
-    ></u-datetime-picker>
-    <!--    列表开始-->
+    <view  v-if="chartData1" style="background-color: #fff ;    padding-top: 4vw; margin-top: 16vw">
+      <view class="charts-box">
+        <qiun-data-charts
+            type="pie"
+            :opts="opts2"
+            :chartData="chartData1"
+        />
+      </view>
+    </view>
 
-    <view style="    padding-top: 61px;">
+    <view style="    margin-top: 5px;">
       <view   @click="jumpDetail(item.months )" class="dingdans_item_rt" v-for="(item,index) in tableData" :key="index">
         <view class="dingdans_top_rt">
           <strong style="margin-left: 12px;" v-if="item.months != '合计'">{{ item.months | dictToDescTypeValue(38) }}</strong>
@@ -152,105 +142,6 @@
         </view>
       </view>
     </view>
-
-<!--    <view1 style="    padding-top: 44px;">-->
-<!--      <view  class="dingdans_item_rt" v-for="(item,index) in tableData" :key="index">-->
-<!--        <view class="dingdans_top_rt">-->
-<!--&lt;!&ndash;          <strong style="margin-left: 12px;">月份：</strong>&ndash;&gt;-->
-<!--&lt;!&ndash;          <strong style="color: #409eff"&ndash;&gt;-->
-<!--&lt;!&ndash;                 > {{item.months}} </strong>&ndash;&gt;-->
-<!--          <strong style="margin-left: 12px;"-->
-<!--                  v-if="item.months == '合计'"> {{item.months}} </strong>-->
-<!--          <strong v-else style="margin-left: 12px;"> {{item.months | dictToDescTypeValue(38)  }} </strong>-->
-<!--        </view>-->
-<!--        <view class="dingdans_con_rt">-->
-<!--          <view style="">-->
-<!--            <strong>-->
-<!--              {{item.successNum}}-->
-<!--            </strong>-->
-<!--            <p>入库数</p>-->
-<!--          </view>-->
-<!--          <view style="">-->
-<!--            <strong>-->
-<!--              {{item.orderAmount}}-->
-<!--            </strong>-->
-<!--            <p>入库总额</p>-->
-<!--          </view>-->
-<!--          <view>-->
-<!--            <strong>-->
-<!--              {{item.profits}}-->
-<!--            </strong>-->
-<!--            <p>已产生利润</p>-->
-<!--          </view>-->
-<!--          <view style="   border-right-width: 0vw;">-->
-<!--            <strong>-->
-<!--              {{item.profitsAmount}}-->
-<!--            </strong>-->
-<!--            <p>市价总额</p>-->
-<!--          </view>-->
-<!--        </view>-->
-<!--        <view class="dingdans_con_rt">-->
-<!--          <view>-->
-<!--            <strong v-if="item.successNum">-->
-<!--              {{item.orderAmount / item.successNum | numFilter}}-->
-<!--            </strong>-->
-<!--            <strong v-else>-->
-<!--              0-->
-<!--            </strong>-->
-<!--            <p>入库均价</p>-->
-<!--          </view>-->
-<!--          <view>-->
-<!--            <strong v-if="item.successNum">-->
-<!--              {{item.profitsAmount / item.successNum | numFilter}}-->
-<!--            </strong>-->
-<!--            <strong v-else>-->
-<!--              0-->
-<!--            </strong>-->
-<!--            <p>市价均价</p>-->
-<!--          </view>-->
-<!--          <view>-->
-<!--            <strong>-->
-<!--              {{item.inventory}}-->
-<!--            </strong>-->
-<!--            <p>剩余库存</p>-->
-<!--          </view>-->
-<!--          <view style="border-right-width: 0vw;">-->
-<!--            <strong>-->
-<!--              {{item.inventoryPrice}}-->
-<!--            </strong>-->
-<!--            <p>剩余总额</p>-->
-<!--          </view>-->
-<!--        </view>-->
-<!--        <view class="dingdans_con_rt">-->
-<!--          <view>-->
-<!--            <strong>-->
-<!--              {{item.saleNum}}-->
-<!--            </strong>-->
-<!--            <p>已售数量</p>-->
-<!--          </view>-->
-<!--          <view>-->
-<!--            <strong>-->
-<!--              {{item.theirPrice}}-->
-<!--            </strong>-->
-<!--            <p>出售金额</p>-->
-<!--          </view>-->
-<!--          <view>-->
-<!--            <strong v-if="item.saleNum">-->
-<!--              {{item.profits / item.saleNum | numFilter}}-->
-<!--            </strong>-->
-<!--            <strong v-else>0</strong>-->
-<!--            <p>利润均价</p>-->
-<!--          </view>-->
-<!--          <view style=" border-right-width: 0vw;">-->
-<!--            <strong>-->
-<!--              {{item.thisTimeProfits}}-->
-<!--            </strong>-->
-<!--            <p>预估利润</p>-->
-<!--          </view>-->
-<!--        </view>-->
-
-<!--      </view>-->
-<!--    </view1>-->
     <!--    列表结束-->
     <view v-show="tableData.length" class="meiyougengduo">
       <u-loadmore fontSize="18"  color="#a6a6a6" nomoreText="最硬球鞋"  loadingText="最硬球鞋" status="nomore"/>
@@ -263,22 +154,31 @@
         textColor="#8a8a8a"
         :icon="$fileUrl +'/static/operateSteps/empity_7.png'">
     </u-empty>
-    <!--    <p style="padding: 0.5rem 0;" class="to-the-bottom">{{emtityMsg}}</p>-->
   </view>
 </template>
 <script>
-  // import Baseline from '@/common/_baseline.vue'
-  // import Footer from '@/common/_footer.vue'
-  // import {reportApi} from '@/api/report'
 
   export default {
-    components: {
-      // 'v-baseline': Baseline,
-      // 'v-footer': Footer
-    },
     name: "HelloWorld",
     data() {
       return {
+        opts2: {
+          color: ["#409eff", "#00c2c2", "#F56C6C", "#FAC858", "#73C0DE", "#3CA272", "#FC8452", "#9A60B4", "#ea7ccc","#5F9EA0"],
+          padding: [5, 5, 5, 5],
+          enableScroll: false,
+          extra: {
+            pie: {
+              activeOpacity: 0.5,
+              activeRadius: 10,
+              offsetAngle: 0,
+              labelWidth: 15,
+              border: false,
+              borderWidth: 3,
+              borderColor: "#FFFFFF"
+            }
+          }
+        },
+        chartData1: {},
         isShowDialog2: false,
         isShowSort: false,
         sortName:'请选择地址',
@@ -353,6 +253,21 @@
         this.$navigateTo(url)
         // this.$router.push({path: '/putinDetail', query: {months}})
       },
+      getName(addressId) {
+        let typeData = this.addressList.filter(item => item.fieldValue == addressId)
+        // return typeData[0].fieldName
+
+        // let typeData = this.addressList.filter(item => item.fieldValue == this.tableData[i].months)
+        if (!typeData || typeData.length == 0) {
+          return null
+        }
+        let name = typeData[0].fieldName
+        if (!name) {
+          return null
+        }
+        name = name.substring(0, name.indexOf('-'))
+        return name
+      },
       getPage() {
         this.allLoaded = false;
         this.$request({
@@ -362,12 +277,41 @@
         }).then(res => {
           if (res.subCode === 1000) {
             this.tableData = res.data ? res.data : [];
-            if (this.tableData.length == 0) {
-              this.emtityMsg = '暂无相关数据';
-              this.allLoaded = true
-            } else {
-              this.emtityMsg = '没有更多了'
+            let data = []
+            for (let i = 0; i < this.tableData.length; i++) {
+              let name = this.getName(this.tableData[i].months)
+              if (!name) {
+                continue
+              }
+              let info = { name: name, value: this.tableData[i].successNum }
+              data.push(info)
             }
+
+            let res1 = {
+              series: [
+                {
+                  data: data
+                }
+              ]
+            };
+            // let res1 = {
+            //   series: [
+            //     {
+            //       data: [
+            //         {"name": "成功数量", "value": this.form.successNum},
+            //         {"name": "库存总数", "value": this.form.inventoryNum}
+            //       ]
+            //     }
+            //   ]
+            // };
+            // let listdata = []
+            // listdata.push(successNum)
+            // listdata.push(inventoryNum)
+            // let res ={}
+            // let series =[]
+            // let series
+
+            this.chartData1 = JSON.parse(JSON.stringify(res1));
           } else {
             this.$toast(res.subMsg)
           }
