71d1b4ee411f0a81397844a672e0209b0e0787f4
CHENXIAOXIN\83745 <873456296@qq.com>
2024-02-19 17:31:59 +0800

    图片

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -728,7 +728,7 @@
       },
       initTime() {
         let myDate = new Date().getTime();
-        let endTime = '2024/02/10 00:00:00';
+        let endTime = '2025/01/29 00:00:00';
         let timestamp2 = Date.parse(new Date(endTime));
         this.seconds = (timestamp2 - myDate) / 1000
         const dates = new Date()
